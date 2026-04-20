61c551a5864b696a049b062d4f26653294112012
DavidR097 <davidalejandrorianosoto@gmail.com>
2026-04-20 15:03:12 -0500

    20/04/2026

---
+++ Migrations/20260420191733_FixPersonal-Solicitudes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Farmacol.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPersonalSolicitudes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
